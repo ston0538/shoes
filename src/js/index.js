d0bd1e7e896e317477479f7054ea2e0f537be010
KHK <ston0538@gmail.com>
2020-10-25 02:42:05 +0900

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,42 @@
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+const title = document.querySelector(".title");
+const sneaker = document.querySelector(".sneaker img");
+const purchase = document.querySelector(".purchase button");
+const description = document.querySelector(".info h3");
+const sizes = document.querySelector(".sizes");
+
+container.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 10;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 10;
+  console.log(xAxis);
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+
+function popIn() {
+  title.style.transform = "translateZ(150px)";
+  sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
+  description.style.transform = "translateZ(125px)";
+  sizes.style.transform = "translateZ(100px)";
+  purchase.style.transform = "translateZ(75px)";
+}
+function popOut() {
+  title.style.transform = "translateZ(0px)";
+  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+  description.style.transform = "translateZ(0px)";
+  sizes.style.transform = "translateZ(0px)";
+  purchase.style.transform = "translateZ(0px)";
+}
+// animate in
+container.addEventListener("mouseenter", (e) => {
+  // mouseleave일때 부드러운 처리를 위해 transition이 처리 되어 있는데
+  // 다시 mouseenter일때 none 처리를 해줘야 transform 움직임에 영향을 주지 않는다.
+  card.style.transition = "none";
+  popIn();
+});
+// animate out
+container.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  popOut();
+});
